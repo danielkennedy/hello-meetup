7cab7cbd8cc10e54e521e1da998ea530d8122284
Dan Kennedy <dan@dankennedy.co>
2015-01-25 20:47:59 -0600

    Added kill route

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # hello-meetup
 App for Cloud Foundry Meetup
+
+## Setting up Lattice locally
+
+## Setting up Lattice on Digital Ocean
+
+## Run the application locally (stand-alone)
+
+## Run the application in Lattice
+
+## Run the application in Cloud Foundry
+
+## Build the Docker image
+
+## Run the Docker image locally (stand-alone)
+
+## Run the Docker image in Lattice
+
+## Run the Docker image in Cloud Foundry
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var kill = require('./routes/kill');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/kill', kill);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
